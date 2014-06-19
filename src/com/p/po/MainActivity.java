c786558c7cde69a45aa359919bd6103da90d91e3
sergio <sergio@onlinestudioproductions.com>
2014-06-19 17:21:15 -0500

    commit 3

--- src/com/p/po/MainActivity.java
+++ src/com/p/po/MainActivity.java
@@ -1,22 +1,15 @@
 package com.p.po;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private List<String> jjj;
-	private TextView tviHola;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        jjj.add("hola");
-        tviHola.setText(jjj.get(0));
         
     }
 
